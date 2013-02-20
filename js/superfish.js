joeldbirch/superfish

commit c1c36a5d11f15eab2b4b24ef1077c755d7d1c12b

Remove setting follow flag to false in hideSuperfishUl().

js/superfish.js
@@ -151,7 +151,6 @@
 						$ul = $(this);
 						$ul.css('visibility','hidden').parent().removeClass(o.hoverClass);
 						o.onHide.call($ul);
-						$$.children('a').data('follow', false);
 					});
 			return this;
 		},

commit 0475c3702e8bf0ed13611bf0147dbfc1b4a045dd

If 'useClick', make link-follow flag false on submenu hide. Fixes IE bug introduced in commit c1c36a5 v1.5.7.
Increment version. Update readme with event delegation note. Update changelog.

js/superfish.js
@@ -1,6 +1,6 @@
 
 /*
- * Superfish v1.5.7 - jQuery menu widget
+ * Superfish v1.5.8 - jQuery menu widget
  * Copyright (c) 2013 Joel Birch
  *
  * Dual licensed under the MIT and GPL licenses:
@@ -151,6 +151,9 @@
 						$ul = $(this);
 						$ul.parent().removeClass(o.hoverClass);
 						o.onHide.call($ul);
+						if (sf.op.useClick){
+							$$.children('a').data('follow', false);
+						}
 					});
 			return this;
 		},