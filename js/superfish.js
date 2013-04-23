4a80cf807cf9beae6f28acf7efbb33f04fad2f16
Joel Birch <joeldbirch@gmail.com>
2013-04-23 15:27:12 +1000

    Fix license whitespace as per jsHint.

--- js/superfish.js
+++ js/superfish.js
@@ -3,8 +3,8 @@
  * Copyright (c) 2013 Joel Birch
  *
  * Dual licensed under the MIT and GPL licenses:
- * 	http://www.opensource.org/licenses/mit-license.php
- * 	http://www.gnu.org/licenses/gpl.html
+ *	http://www.opensource.org/licenses/mit-license.php
+ *	http://www.gnu.org/licenses/gpl.html
  */
 
 ;(function($) {
